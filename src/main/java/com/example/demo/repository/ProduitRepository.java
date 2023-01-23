4a5d9320e3274d851bcbc0c3e557a528ece0f45f
Achraf Taffah <99338171+TAFFAHACHRAF@users.noreply.github.com>
2023-01-23 18:42:49 +0100

    Delete src directory

--- src/main/java/com/example/demo/repository/ProduitRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.modele.Produit;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProduitRepository extends JpaRepository<Produit,Long> {
-}
