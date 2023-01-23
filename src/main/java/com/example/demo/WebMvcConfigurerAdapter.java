ae4cd63458b584a53e7d9e8584c55a04060a6abb
Achraf Taffah <99338171+TAFFAHACHRAF@users.noreply.github.com>
2023-01-23 18:47:20 +0100

    Delete src directory

--- src/main/java/com/example/demo/WebMvcConfigurerAdapter.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo;
-
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-public class WebMvcConfigurerAdapter implements WebMvcConfigurer {
-}
--- src/main/java/com/example/demo/repository/EmployeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.modele.Employe;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeRepository extends JpaRepository<Employe,Long> {
-}
